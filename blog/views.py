ea3385704df88cb70eade3c0bf4cf13cf5afc84f
yahanjunho <yahanjunho@gmail.com>
2021-06-04 14:45:47 +0900

    update, drafts

--- blog/views.py
+++ blog/views.py
@@ -33,7 +33,7 @@ def post_new(request):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
+      #post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
 
@@ -54,7 +54,7 @@ def post_edit(request, pk):
     if form.is_valid():
       post = form.save(commit=False)
       post.author = request.user
-      post.published_date = timezone.now()
+      #post.published_date = timezone.now()
       post.save()
       return redirect('post_detail', pk=post.pk)
 
@@ -65,3 +65,14 @@ def post_edit(request, pk):
   return render(request, 'blog/post_edit.html', {
     'form': form,
   } )  
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
